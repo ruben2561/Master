2afce798058a54a1aaea856942766b67a409e767
ruben godde <ruben.godde@student.uhasselt.be>
2024-04-03 10:36:53 +0200

    changed data structure

--- solcast.py
+++ solcast.py
@@ -28,7 +28,7 @@ print("sexyyy") """
 
 def get_sample_data():
     try:
-        with open("sampleData/pv_data.csv", "r") as file:
+        with open("sampleData/pv_data_day.csv", "r") as file:
             sample_data = file.read()
             return sample_data
     except Exception as e:
@@ -81,41 +81,3 @@ def get_solar_radiation_forecast(latitude, longitude, start_date, end_date):
 #    print(forecast_data)
 #else:
 #    print("Failed to retrieve solar radiation forecast.")
-    
-
-
-
-
-# Specify the time for the forecast (in UTC)
-# forecast_time = "2023-11-13T12:00:00Z"  # Replace with your desired time
-
-# # Solcast API endpoint for Global Horizontal Irradiance (GHI) forecast
-# endpoint = f"https://api.solcast.com.au/data/forecast/radiation_and_weather?latitude=-33.856784&longitude=151.215297&api_key=w58k5_UUO4JxNP3ykI-gsRn8w65hJQQQ"
-# endpoint2 = f"https://api.solcast.com.au/data/forecast/radiation_and_weather?latitude={latitude}&longitude={longitude}&api_key={api_key}"
-# endpoint3 = f"https://api.solcast.com.au/data/forecast/radiation_and_weather?latitude=-33.856784&longitude=151.215297&output_parameters=air_temp,dni,ghi&format=json&api_key={api_key}"
-# endpoint4 = f"https://api.solcast.com.au/data/historic/radiation_and_weather?latitude=-33.856784&longitude=151.215297&azimuth=44&tilt=90&start=2023-10-25T14:45:00.000Z&duration=P31D&format=csv&time_zone=utc&api_key={api_key}"
-# endpoint5 = "https://api.solcast.com.au/data/historic_forecast/radiation_and_weather?latitude=48.30783&longitude=-105.1017&azimuth=44&tilt=90&start=2022-10-25T14:45:00.000Z&duration=P31D&format=csv&time_zone=utc&lead_time=PT1H"
-# endpoint6 = "https://api.solcast.com.au/rooftop_sites/2232-a658-4143-f02f/estimated_actuals?format=csv&api_key=w58k5_UUO4JxNP3ykI-gsRn8w65hJQQQ"
-# endpoint7 = "https://api.solcast.com.au/weather_sites/f0c1-3849-a865-4bca/estimated_actuals?format=json&api_key=w58k5_UUO4JxNP3ykI-gsRn8w65hJQQQ"
-
-# url = endpoint6
-
-# print(url + "\r\r")
-# try:
-
-#     payload={}
-#     headers = {}
-
-#     response = requests.request("GET", url, headers=headers, data=payload)
-
-#     # Check if the request was successful (status code 200)
-#     print(response.text)
-#     if response.status_code == 200:
-#         data = response.json()
-#         print("Solar Radiation Data:")
-#         print(data)
-#     else:
-#         print(f"Error: {response.status_code}, {response.text}")
-
-# except Exception as e:
-#     print(f"Error: {e}")
