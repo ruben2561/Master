f66c78008a272418ddadd1b216d7d9d30f0c1a57
ruben godde <ruben.godde@student.uhasselt.be>
2024-04-05 14:27:47 +0200

    added function to calculate injection and extraction prices

--- processPrices.py
+++ processPrices.py
@@ -4,16 +4,26 @@ import numpy as np
 from APIEntsoe import get_prices_data
 
 def Calculate_injection_and_extraction_prices(pricing_data):
-    prices_grid = []
+    prices_grid = [float(data_point["price"]) for data_point in pricing_data if float(data_point["price"]) >= 0]
+    average_price = sum(prices_grid) / len(prices_grid)
     
-    for data_point in pricing_data:
-        prices_grid.append(float(data_point["price"]))
+    prices_grid = [float(data_point["price"]) if float(data_point['price']) > 10 else 10 for data_point in pricing_data]
     
-    prices_injection = [(0.85 * x) - 4 for x in prices_grid]
-    prices_extraction = [(1.02 * x) + 4 for x in prices_grid]
+    prices_injection = []
+    prices_extraction = []
     
-    return prices_injection, prices_extraction
-
+    for x in prices_grid:
+        if x >= 10:
+            prices_injection.append(0.85 * x - 4)
+            prices_extraction.append(1.02 * x + 4)
+        else:
+            prices_injection.append(0.85 * 10 - 4)
+            prices_extraction.append(1.02 * 10 + 4)
+    
+    return {
+        "prices_injection": prices_injection, 
+        "prices_extraction": prices_extraction,
+    }
 
 def process_prices_data(data_points, latitude, longitude):
     prices_data = get_prices_data(
@@ -24,10 +34,10 @@ def process_prices_data(data_points, latitude, longitude):
     prices_list = list(reader)
     
     #pv_data = calculate_pv_power(forecast_list, latitude, longitude, capacity_kW, number_of_panels, azimuth, tilt, efficiency_panels, efficiency_invertor)
-    prices_injection, prices_extraction = Calculate_injection_and_extraction_prices(prices_list)
+    prices = Calculate_injection_and_extraction_prices(prices_list)
     
     for i in range(len(data_points) - 1):
-        data_points[i]["price_injection"] = float(prices_injection[i])
-        data_points[i]["price_extraction"] = float(prices_extraction[i])
-        
+        data_points[i]["price_injection"] = float(prices["prices_injection"][i])
+        data_points[i]["price_extraction"] = float(prices["prices_extraction"][i])
+    
     return data_points
